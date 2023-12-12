5ce246e101b7d006a28b106abf1206afcd28a4d3
Andre Rousselet <andre098@hotmail.com>
2023-12-12 18:08:33 -0300

    feat(apps/web): remove default docs and web from apps and add new latest next-app

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@repo/ui/card";
+
+export default function Home() {
+  return (
+    <div>
+      <Card title="Hello World" href="/">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eos
+        modi, facilis laborum inventore quaerat, necessitatibus doloribus
+        numquam, iure reiciendis veniam voluptate? Ipsa perferendis laborum id
+        tempora? Corporis, nesciunt voluptatibus?a
+      </Card>
+    </div>
+  );
+}
